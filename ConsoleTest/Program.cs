8ee1790564afd2b49531b858c0c91aeaf0582eb9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-24 21:39:50 +0700

    Initial commit

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using LibVLCSharp.Shared;
+
+namespace ConsoleTest
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			Console.WriteLine("Now Playing: JJD & Alex Skrindo - Elation");
+			Core.Initialize();
+			LibVLC libVLC = new LibVLC("--input-repeat=2");
+			MediaPlayer mp = new MediaPlayer(libVLC)
+			{
+				Media = new Media(libVLC, new Uri("https://upload.wikimedia.org/wikipedia/commons/7/79/Example_of_flats_in_music.ogg"))
+			};
+			mp.Play();
+			mp.Volume = 100;
+			Console.ReadKey();
+		}
+	}
+}
